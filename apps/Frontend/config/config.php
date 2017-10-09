08866c6b22d65b01c94064b2a8641f1fb9bd5b79
Adrien Mahieux <adrien.mahieux@gmail.com>
2017-10-09 10:21:13 +0200

    [Add] Add logging configuration

--- apps/Frontend/config/config.php
+++ apps/Frontend/config/config.php
@@ -12,8 +12,26 @@ return new \Phalcon\Config([
 		'host'        => 'localhost',
 		'username'    => 'root',
 		'password'    => '',
-		'dbname'      => 'dataengine',
+		'dbname'      => 'amportal',
 		'charset'     => 'utf8',
+	],
+	'logger' => [
+		'localfile' => [
+			'type'		=> 'File',
+			'param1'	=> BASE_PATH . '/data/logs/app.'.APP_TYPE.'.'.date('ymd').'.log',
+			'param2'	=> null,
+			// For levels, see logger.zep https://github.com/phalcon/cphalcon/blob/master/phalcon/logger.zep
+			'level'		=> 7, // SPECIAL=9, CUSTOM, DEBUG=7, INFO, NOTICE, WARNING, ERROR, ALERT, CRITICAL, EMERGENCY = 0
+			'lineFmt'	=> null,
+			'dateFmt'	=> 'y-m-d_H-m-s',
+
+		],
+		/*
+		'syslog' => [
+			'type'		=> 'Syslog',
+			'param'		=> 
+		]
+		*/
 	],
 	'application' => [
 		'viewsDir'    => __DIR__ . '/../Views/',
