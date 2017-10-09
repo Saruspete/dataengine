79a0e5aa5074f14e5c49b6d9b606f3d03695c021
Adrien Mahieux <adrien.mahieux@gmail.com>
2017-10-09 10:25:10 +0200

    [Add] css and js assets

---
+++ public/js/common.js
@@ -0,0 +1,10 @@
+// Auto-Dropdown for navbar
+/*
+$(document).ready(function () {
+$('.navbar .dropdown').hover(function () {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
+	}, function () {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideUp(105)
+	});
+});
+*/
